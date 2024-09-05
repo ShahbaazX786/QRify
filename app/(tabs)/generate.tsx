98077b29607711431523f65812e6e7564d8e003b
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-09-05 16:42:04 +0530

    added new tabs

---
+++ app/(tabs)/generate.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function GenerateQR() {
+  return (
+    <View className="flex flex-row justify-center items-center">
+      <Text className="text-4xl">Bismillah</Text>
+    </View>
+  );
+}
+
+export default GenerateQR;
