8fe28d98642987ed36b7e7747f315b5063a0da6b
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-09-06 18:12:34 +0530

    did some minor modifications with built in icon lib.

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import WaveBottom from "@/assets/svg/WaveBottom";
 import { default as WaveTop } from "@/assets/svg/waveTop";
 import BeginButton from "@/components/BeginButton";
+import { AntDesign } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 import React from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
@@ -12,19 +13,14 @@ const Welcome = () => {
     <SafeAreaView className=" flex-1 bg-blue-800 relative">
       <WaveTop className="w-full h-36 opacity-100 stroke-black shadow-xl absolute top-0 left-0 z-10" />
       <View className="bg-blue-500 w-full h-full">
-        <View className="mt-auto">
-          <Image
-            source={require("../assets/images/qr_code.png")}
-            alt="Qrify"
-            resizeMode="contain"
-            className="w-full"
-          />
-          <Text className="mt-8 text-center text-white text-6xl font-bold mb-5">
+        <View className="mt-auto mx-auto">
+          <AntDesign name="qrcode" size={200} color="white" />
+          <Text className="mt-2 text-center text-white text-6xl font-bold mb-5">
             QRify
           </Text>
         </View>
 
-        <Text className="text-center text-lg text-white mb-12 mt-auto mx-8">
+        <Text className="text-center text-lg font-bold text-white mb-8 mt-auto mx-8">
           Discover the best way to Create and share QR codes effortlessly, like
           never before
         </Text>
