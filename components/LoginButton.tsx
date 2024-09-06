3cff7afd06b2d7688e2cf2b7ab404ad53698289d
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-09-06 18:05:09 +0530

    style modifications for login button

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -3,7 +3,7 @@ import { Text, TouchableOpacity } from "react-native";
 
 const LoginButton = () => {
   return (
-    <TouchableOpacity className="mr-4">
+    <TouchableOpacity className="mr-2 border px-2 py-1">
       <Text className="text-lg text-black">Login</Text>
     </TouchableOpacity>
   );
