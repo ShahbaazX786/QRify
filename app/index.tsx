0e180675419ca32de3ae2936b38a7db5a0ece6ba
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-09-07 15:45:17 +0530

    Animated the component view a little bit and then utilized clerk auth methods.

--- app/index.tsx
+++ app/index.tsx
@@ -1,30 +1,41 @@
 import WaveBottom from "@/assets/svg/WaveBottom";
-import { default as WaveTop } from "@/assets/svg/waveTop";
+import WaveTop from "@/assets/svg/waveTop";
 import BeginButton from "@/components/BeginButton";
+import { useAuth } from "@clerk/clerk-expo";
 import { AntDesign } from "@expo/vector-icons";
-import { useRouter } from "expo-router";
+import { Redirect, useRouter } from "expo-router";
 import React from "react";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import Animated, { FadeInDown } from "react-native-reanimated";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Welcome = () => {
+  const { isSignedIn } = useAuth();
   const router = useRouter();
+
+  if (isSignedIn) {
+    return <Redirect href={"/(tabs)/generate"} />;
+  }
+
   return (
-    <SafeAreaView className=" flex-1 bg-blue-800 relative">
+    <SafeAreaView className="bg-blue-800 relative justify-center items-center flex flex-col">
       <WaveTop className="w-full h-36 opacity-100 stroke-black shadow-xl absolute top-0 left-0 z-10" />
-      <View className="bg-blue-500 w-full h-full">
-        <View className="mt-auto mx-auto">
+      <View className="bg-blue-500 py-28">
+        <Animated.View
+          entering={FadeInDown.springify().delay(100)}
+          className="flex justify-center items-center space-y-4 flex-1"
+        >
           <AntDesign name="qrcode" size={200} color="white" />
-          <Text className="mt-2 text-center text-white text-6xl font-bold mb-5">
+          <Text className="text-center text-white text-6xl font-bold">
             QRify
           </Text>
-        </View>
+        </Animated.View>
 
-        <Text className="text-center text-lg font-bold text-white mb-8 mt-auto mx-8">
-          Discover the best way to Create and share QR codes effortlessly, like
-          never before
-        </Text>
-        <View className="mb-auto">
+        <View className="space-y-8">
+          <Text className="text-center text-lg font-bold text-white px-8">
+            Discover the best way to Create and share QR codes effortlessly,
+            like never before
+          </Text>
           <TouchableOpacity onPress={() => router.replace("/(tabs)/generate")}>
             <BeginButton title={"get started"} />
           </TouchableOpacity>
