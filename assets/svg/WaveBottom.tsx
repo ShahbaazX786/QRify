58860206af0244b5fab8607380542ba8365f4907
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-09-07 15:43:53 +0530

    minor import refactoring.

--- assets/svg/WaveBottom.tsx
+++ assets/svg/WaveBottom.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import Svg, { SvgProps, Path } from "react-native-svg";
 const WaveBottom = (props: SvgProps) => (
   <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" {...props}>
